93e099ab0f91c14376fb3a44546f1f286d91f7c8
Jordan Thomas <jthomas135@hotmail.com>
2022-04-08 10:35:09 -0700

    testing that my commits are working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,55 @@
 // Assignment code here
-var numArr = ['1234567890']
-
-var upperArr = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-
-var lowerArr = ["abcdefghijklmnopqrstuvwxyz"]
-
-var specialArr = ["!@#$%^&*()-+=_[{}]:;<>.?/~`"]
-
-function generatePassword() {
-
-  var useLowers = confirm(
-    "Would you like to use Lowercase letters?"
-    );
-
-  var useUppers = confirm(
-    "Would you like to use Uppercase letters?"
-    );
-
-  var useNums = confirm(
-    "Would you like to use Numbers?"
-    );
-          
-  var useSpecials = confirm(
-    "Would you like to use Special characters?"
-    );
-
-  var passwordLength = prompt(
-    "How long would you like the password to be? Enter a number from 8 to 128."
-    );
-        
-  passwordLength = parseInt(passwordLength)
-  
-  if (passwordLength >= 8 && passwordLength <= 128) {
-    // use number of characters selected
-  }
-  
-  else {
-    alert("Please select a valid number of characters!")
-    // send back to passwordLength prompt
-  }
-
-  if (!useLowers && !useUppers && !useNums && !useSpecials) {
-    alert("Please select at least one character type to use!")
-    //send back to start of prompts
-  }
-
-  else {
-    // use variables selected
-  }
+// Create a single variable containing character options to be called upon later in the code
+const characterKey = {
+  numArr: '1234567890',
+  upperArr: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerArr: "abcdefghijklmnopqrstuvwxyz",
+  specialArr: "!@#$%^&*()-+=_[{}]:;<>.?\/~`"
 };
 
+function generatePassword() {
+  
+  var passwordCharacters = "";
+
+    //ask how long the user wants the password to be
+
+    var promptLength = prompt("How long would you like the password to be? Enter a number from 8 to 128.");
+
+    if (promptLength <= 8 && promptLength >= 128) {
+      alert("Please select a valid number of characters!");
+    };
+
+      // Confirmation prompts for each type of character option
+      var promptLower = confirm("Would you like to use Lowercase letters?");
+
+        //validating response and choosing to use the array or not
+        if (promptLower) {
+          passwordCharacters += characterKey.lowerArr;
+        };
+
+      var promptUpper = confirm("Would you like to use Uppercase letters?");
+        
+        //Using if to decide how to react to prompt entry
+        if (promptUpper) {
+          passwordCharacters += characterKey.upperArr;
+        };
+
+      var promptNum = confirm("Would you like to use Numbers?");
+          
+        //Using if to decide how to react to prompt entry
+        if (promptNum) {
+         passwordCharacters += characterKey.numArr;
+        };
+        
+      var promptSpecial = confirm("Would you like to use Special characters?");
+
+        //Using if to decide how to react to prompt entry
+        if (promptSpecial) {
+         passwordCharacters += characterKey.specialArr;
+        };
+};
+
+console.log(generatePassword());
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
